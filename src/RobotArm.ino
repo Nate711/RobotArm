2f1c375c6d2e465e17ce5f011d2e63897088dc19
Nathan Kau <nathankau@gmail.com>
2016-04-05 00:27:04 -0700

    added code to move fence

--- src/RobotArm.ino
+++ src/RobotArm.ino
@@ -4,11 +4,10 @@
 
 //Servo pins (digital)
 #define GRIPPER 5
-// NOT UPLOADED YET
-
 #define WRIST 6
 #define ELBOW 7
 #define SHOULDER 8
+#define FENCE 4
 
 #define BASE_POT A0
 #define SHOULDER_POT A1
@@ -19,6 +18,8 @@
 #define BASE_INCR 10 // pins to incr/decr base stepper
 #define BASE_DECR 11
 
+#define FENCE_BUTTON 9
+
 #define MAX_SPEED 10000
 #define MAX_ACCEL 5000
 
@@ -28,6 +29,8 @@ double shoulder;
 double elbow;
 double wrist;
 double gripper;
+
+double fence=90;
 
 const unsigned int mem_size = 10;
 unsigned int base_pot_mem[mem_size];
@@ -44,9 +47,13 @@ Servo elbow_servo;
 Servo wrist_servo;
 Servo gripper_servo;
 
+Servo fence_servo;
+
 long last_servo_write = 0;
 long last_base_offset = 0;
 long last_step = 0;
+
+bool last_button_state = false; // false = unpressed, true = pressed
 
 int activated = 1; // not using activation button
 
@@ -60,6 +67,8 @@ void setup() {
 
   gripper_servo.attach(GRIPPER);
 
+  fence_servo.attach(FENCE);
+
   base_stepper.setMaxSpeed(MAX_SPEED);
   base_stepper.setAcceleration(MAX_ACCEL);
 
@@ -71,6 +80,7 @@ void setup() {
 
   pinMode(BASE_INCR,INPUT_PULLUP);
   pinMode(BASE_DECR,INPUT_PULLUP);
+  pinMode(FENCE_BUTTON,INPUT_PULLUP);
 
   pinMode(13,OUTPUT);
   digitalWrite(13,HIGH);
@@ -114,7 +124,7 @@ void readPots() {
   // jitter doesn't go away when hardcode elbow
   // elbow still jitters when connected to gripper digital output pin
   wrist = map(wristV,64,936,180,0);
-  gripper = map(gripperV,0,1024,0,180); // calibrated for full open/close
+  gripper = map(gripperV,0,1024,0,120); // calibrated for full open/close
 }
 
 int average_array(unsigned int *array, int size) {
@@ -132,11 +142,21 @@ void writeServos() {
   base_servo.write(base);
 
   gripper_servo.write(gripper);
+
+  fence_servo.write(fence);
 }
 
 // with teensy put this on a timer
 void writeStepper() {
   base_stepper.run();
+}
+
+void moveFence() {
+  bool new_button_state = digitalRead(FENCE_BUTTON) == LOW;
+  if(!new_button_state & last_button_state) { // button is now unpressed but was pressed before
+    fence = (fence == 90) ? 3  : 90; // toggle between 0 and 90
+  }
+  last_button_state = new_button_state;
 }
 
 void offsetBase(double offset) {
@@ -179,6 +199,7 @@ void loop() {
       /************/
       readPots();
       writeServos();
+      moveFence();
       last_servo_write = millis();
     }
     // 30hz
@@ -186,8 +207,8 @@ void loop() {
       offsetBase(2.0);
       last_base_offset = millis();
     }
-    //As fast as possibley ns
-    if(micros() - last_step > 100) {
+    //As fast as possible
+    if(micros() - last_step > 50) {
       writeStepper();
       last_step = micros();
     }
